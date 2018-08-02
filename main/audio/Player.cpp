d5ed0dda6dae90975e817025ac87c1383ffb4efb
Felipe Makara <fdmakara@gmail.com>
2018-08-01 21:07:03 -0300

    Updating ide and player

--- main/audio/Player.cpp
+++ main/audio/Player.cpp
@@ -67,7 +67,7 @@ void Player::play(){
 
 void Player::setup_triangle_sine_waves(int bits){
 	i2s_bits_per_sample_t bps;
-    int *samples_data = (int*)malloc(((bits+8)/16)*SAMPLE_PER_CYCLE*4);
+    short samples_data[2*SAMPLE_PER_CYCLE*4];
     unsigned int i, sample_val;
     double sin_float, triangle_float, triangle_step = (double) pow(2, bits) / SAMPLE_PER_CYCLE;
 
@@ -75,32 +75,20 @@ void Player::setup_triangle_sine_waves(int bits){
 
     triangle_float = -(pow(2, bits)/2 - 1);
 
-    for(i = 0; i < SAMPLE_PER_CYCLE; i++) {
-        sin_float = (2<<(bits-3))*(1.0+sin(i *2* PI / (SAMPLE_PER_CYCLE)));
+    for(i = 0; i < SAMPLE_PER_CYCLE*4; i++) {
+        sin_float = (2<<(bits-2))*(1.0+sin(i *2* PI / (SAMPLE_PER_CYCLE)));
         if(sin_float >= 0)
             triangle_float += triangle_step;
         else
             triangle_float -= triangle_step;
 
 
-        if (bits == 16) {
-            sample_val = 0;
-            sample_val += 0;
-            sample_val = sample_val << 16;
-            sample_val += (short)(sin_float);
-            samples_data[i] = sample_val;
-            printf("%d,%d\n",i,samples_data[i]);
-        } else if (bits == 24) { //1-bytes unused
-            samples_data[i*2] = ((int) sin_float) << 8;
-            samples_data[i*2 + 1] = ((int) sin_float) << 8;
-        } else {
-            samples_data[i*2] = ((int) sin_float);
-            samples_data[i*2 + 1] = ((int) sin_float);
-        }
+		samples_data[i*2+0] = (short)(sin_float);
+		samples_data[i*2+1] = (short)(sin_float);
 
     }
 
-    switch(bits){
+    switch(bits){ //11.2ms escritos, 3ms cheio, 8.2ms vazio
     case 8:bps = I2S_BITS_PER_SAMPLE_8BIT;break;
     case 16:bps = I2S_BITS_PER_SAMPLE_16BIT;break;
     case 24:bps = I2S_BITS_PER_SAMPLE_24BIT;break;
@@ -117,7 +105,9 @@ void Player::setup_triangle_sine_waves(int bits){
     //         i2s_push_sample(0, &samples_data[i*2], 100);
     // }
     // or write
-    i2s_write_bytes(I2S_NUM_0, (const char *)samples_data, ((bits+8)/16)*SAMPLE_PER_CYCLE*4, (TickType_t)100);
+    i2s_zero_dma_buffer(I2S_NUM_0);
+    size_t written=0;
+    i2s_write(I2S_NUM_0, (const void *)samples_data, sizeof(samples_data), 2048, (TickType_t)portMAX_DELAY);
+    printf("%d wr\n",written);
 
-    free(samples_data);
 }
